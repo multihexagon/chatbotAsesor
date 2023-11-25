210e5f92546abb4d019ff3fb4045562f590528af
multihexagon <98978765+multihexagon@users.noreply.github.com>
2023-11-25 14:50:26 -0500

    cambio a estilo wpp

--- asesor/src/components/chat.jsx
+++ asesor/src/components/chat.jsx
@@ -1,18 +1,28 @@
 import React, { useState, useEffect, useRef } from 'react';
+import './chat.css';
 
 const Chat = () => {
-  const [messages, setMessages] = useState([
-    { sender: 'Usuario', text: 'Hola buen día, tengo un problema con mi suscripción!' },
-    { sender: 'Tú', text: 'Hola buenos dias, claro que sí, será un placer ayudarte' },
-  ]);
+  const [activeChat, setActiveChat] = useState('Chat1'); // Default active chat
+  const [chats, setChats] = useState({
+    Chat1: [
+      { sender: 'Usuario', text: 'Hello! How can I help you today?' },
+      { sender: 'You', text: 'Hi there! I have a question.' },
+    ],
+    Chat2: [
+      { sender: 'Usuario', text: 'Welcome to Chat 2!' },
+      { sender: 'You', text: 'How can I assist you?' },
+    ],
+    // Add more chats as needed
+  });
 
   const messagesContainerRef = useRef(null);
 
   const renderMessages = () => {
-    return messages.map((message, index) => (
+    const currentChat = chats[activeChat] || [];
+    return currentChat.map((message, index) => (
       <div
         key={index}
-        style={{ marginBottom: '10px', textAlign: message.sender === 'Usuario' ? 'left' : 'right', backgroundColor: message.sender === 'Usuario' ? '#eee' : '#0084ff', color: message.sender === 'Usuario' ? '#000' : '#fff', padding: '10px', borderRadius: '5px' }}
+        style={{ marginBottom: '10px', textAlign: message.sender === 'Usuario' ? 'left' : 'right', backgroundColor: message.sender === 'Usuario' ? '#eee' : '#375EFC', padding: '5px', borderRadius: '5px', color: message.sender === 'Usuario' ? '#000' : '#fff' }}
       >
         <strong>{message.sender}:</strong> {message.text}
       </div>
@@ -21,40 +31,78 @@ const Chat = () => {
 
   const handleSendMessage = () => {
     const inputElement = document.getElementById('messageInput');
-    const newMessage = { sender: 'Tú', text: inputElement.value };
-    setMessages([...messages, newMessage]);
-    inputElement.value = ''; 
+    const currentChat = chats[activeChat] || [];
+    const newMessage = { sender: 'You', text: inputElement.value };
+    setChats({ ...chats, [activeChat]: [...currentChat, newMessage] });
+    inputElement.value = ''; // Clear the input field
   };
 
   useEffect(() => {
-
+    // Scroll to the bottom when messages change
     messagesContainerRef.current.scrollTop = messagesContainerRef.current.scrollHeight;
-  }, [messages]);
+  }, [chats, activeChat]);
 
-  return (
-    <div style={{ maxWidth: '500px', margin: 'auto', fontFamily: 'Arial, sans-serif' }}>
+  const handleTabClick = (chatId) => {
+    setActiveChat(chatId);
+  };
+
+  const renderTabs = () => {
+    return Object.keys(chats).map((chatId) => (
       <div
-        ref={messagesContainerRef}
+        key={chatId}
+        onClick={() => handleTabClick(chatId)}
         style={{
-          height: '300px',
-          border: '1px solid #ccc',
-          borderRadius: '5px',
-          overflowY: 'scroll',
           padding: '10px',
+          borderBottom: activeChat === chatId ? '2px solid #4CAF50' : '2px solid transparent',
+          cursor: 'pointer',
         }}
       >
-        {renderMessages()}
+        {chatId}
       </div>
-      <div style={{ marginTop: '10px', display: 'flex' }}>
-        <input
-          id="messageInput"
-          type="text"
-          placeholder="Type your message..."
-          style={{ flex: '1', marginRight: '10px', padding: '5px' }}
-        />
-        <button style={{ padding: '5px', cursor: 'pointer' }} onClick={handleSendMessage}>
-          Send
-        </button>
+    ));
+  };
+
+  return (
+    <div style={{ display: 'flex' }}>
+      {/* Sidebar with tabs */}
+      <div style={{ width: '200px', borderRight: '1px solid #ccc' }}>
+        {renderTabs()}
+      </div>
+      {/* Chat messages */}
+      <div style={{ flex: '1', maxWidth: '400px', margin: 'auto', fontFamily: 'Arial, sans-serif' }}>
+        <div
+          ref={messagesContainerRef}
+          style={{
+            height: '300px',
+            border: '1px solid #ccc',
+            borderRadius: '5px',
+            overflowY: 'scroll',
+            padding: '10px',
+          }}
+        >
+          {renderMessages()}
+        </div>
+        <div style={{ marginTop: '10px', display: 'flex' }}>
+          <input
+            id="messageInput"
+            type="text"
+            placeholder="Type your message..."
+            style={{ flex: '1', marginRight: '10px', padding: '5px' }}
+          />
+          <button style={{ padding: '5px', cursor: 'pointer' }} className='button' onClick={handleSendMessage}>
+          <svg  height="24"
+                width="24"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M0 0h24v24H0z" fill="none"></path>
+                <path
+                  d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+          </button>
+        </div>
       </div>
     </div>
   );
