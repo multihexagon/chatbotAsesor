006db5d8008de9255e15bc8f21327d3f0e205bb6
Juan Pablo Sanchez Villegas <juanpabl0svn@gmail.com>
2023-11-28 02:34:23 -0500

    better design and helped with things

--- src/components/messages/messages.jsx
+++ src/components/messages/messages.jsx
@@ -1,19 +1,16 @@
+import "./messages.css";
+
 const RenderMessages = ({ chats }) => {
-  return chats?.messages ? chats.messages.map((message, index) => (
-    <div
-      key={index}
-      style={{
-        marginBottom: "10px",
-        textAlign: message.from === "Usuario" ? "left" : "right",
-        backgroundColor: message.from === "Usuario" ? "#eee" : "#9966CC",
-        color: message.from === "Usuario" ? "#000" : "#fff",
-        padding: "10px",
-        borderRadius: "5px",
-      }}
-    >
-      <strong>{message.from}:</strong> {message.text}
-    </div>
-  )) : null;
+  return chats?.messages
+    ? chats.messages.map((message, index) => (
+        <div
+          key={index + message.text}
+          className={`chat-message ${message.from === "user" ? "user" : ""}`}
+        >
+          {message.text}
+        </div>
+      ))
+    : null;
 };
 
 export default RenderMessages;
