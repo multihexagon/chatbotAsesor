027eb900c83744565470ceab78c45732e92cd859
Juan Pablo Sanchez Villegas <juanpabl0svn@gmail.com>
2023-11-29 00:38:16 -0500

    auth

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -13,21 +13,51 @@ function Login() {
     }
   }, []);
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
-    setChatData((lastValue) => ({ ...lastValue, loggedIn: "pepito" }));
-    localStorage.setItem("token", "pepito");
+
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const password = formData.get("password");
+
+    const req = await fetch("http://localhost:3000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (!req.ok) {
+      alert("mal");
+      return;
+    }
+    const { token } = await req.json();
+
+    setChatData((lastValue) => ({ ...lastValue, loggedIn: token }));
+    localStorage.setItem("token", token);
   }
 
   return (
     <form className="form" onSubmit={handleSubmit}>
-      <p className="form-title">Sign in to your account</p>
+      <p className="form-title">Inicia sesión para chatear como asesor</p>
       <div className="input-container">
-        <input type="email" placeholder="Enter email" />
+        <input
+          type="text"
+          placeholder="Enter username"
+          name="username"
+          required
+        />
         <span></span>
       </div>
       <div className="input-container">
-        <input type="password" placeholder="Enter password" />
+        <input
+          type="password"
+          placeholder="Enter password"
+          name="password"
+          required
+        />
       </div>
       <button type="submit" className="submit">
         Sign in
